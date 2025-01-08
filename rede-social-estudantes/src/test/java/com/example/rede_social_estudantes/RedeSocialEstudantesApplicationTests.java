7c0fc3167086798ac5784f06781be8e018fad22d
Maria dutra <dutracosta1904@gmail.com>
2025-01-08 00:59:10 -0300

    add the spring boot

---
+++ rede-social-estudantes/src/test/java/com/example/rede_social_estudantes/RedeSocialEstudantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rede_social_estudantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialEstudantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
